701e72f1c50a72551e4b0301110d2b34cbd668fc
AntonChetveryk <chetverykanton92@gmail.com>
2020-01-09 17:47:39 +0200

    functional settate

--- src/components/AutoRefresh.jsx
+++ src/components/AutoRefresh.jsx
@@ -8,16 +8,18 @@ export default class AutoRefresh extends React.Component {
     };
   }
 
-  setAutoRefreshing = () => {
-    this.setState(prevState => {
-      return { isAutoRefreshing: !prevState.isAutoRefreshing };
-    });
-
+  intervalHandler = () => {
+    console.log(this.state.isAutoRefreshing);
     let interval = setInterval(this.props.getData, 3000);
-
     if (!this.state.isAutoRefreshing) {
       clearInterval(interval);
     }
+  };
+
+  setAutoRefreshing = () => {
+    this.setState(prevState => {
+      return { isAutoRefreshing: !prevState.isAutoRefreshing };
+    }, this.intervalHandler);
   };
 
   render() {
