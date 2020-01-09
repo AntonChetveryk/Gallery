2ffb8cdf30b281a23fd895c3a80e30037983ee9d
AntonChetveryk <chetverykanton92@gmail.com>
2020-01-09 22:15:35 +0200

    last commit

--- src/components/ItemsList.jsx
+++ src/components/ItemsList.jsx
@@ -8,16 +8,18 @@ export default class ItemsList extends React.Component {
 
     return (
       <>
-        {data
-          .filter(item => item.data.num_comments >= num_comments)
-          .sort((a, b) => b.data.num_comments - a.data.num_comments)
-          .map((item, i) => {
-            return (
-              <div key={i} className="item">
-                <Item item={item} />
-              </div>
-            );
-          })}
+        {data.length
+          ? data
+              .filter(item => item.data.num_comments >= num_comments)
+              .sort((a, b) => b.data.num_comments - a.data.num_comments)
+              .map((item, i) => {
+                return (
+                  <div key={i} className="item">
+                    <Item item={item} />
+                  </div>
+                );
+              })
+          : "No results found matching your criteria."}
       </>
     );
   }
